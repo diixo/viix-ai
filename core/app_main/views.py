b6e42fda4b92540cf088cd705ab1edbb50152fc9
diixo <anatoliy.podoba@dxc.com>
2025-01-19 06:57:13 +0200

    views.py main

--- core/app_main/views.py
+++ core/app_main/views.py
@@ -1,8 +1,8 @@
 
 from django.shortcuts import render, redirect
 
-# def main(request):
-#     return redirect('app_main:ai-search')
+def main(request):
+    return render(request, "app_main/index.html", context={})
 
 
 def ai_search(request):
