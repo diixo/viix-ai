b0f8014d421fe9bdf082176afb7a982c1055c81e
diixo <anatoliy.podoba@dxc.com>
2025-01-19 02:27:05 +0200

    Added ai-search

--- core/app_main/urls.py
+++ core/app_main/urls.py
@@ -8,4 +8,5 @@ app_name = "app_main"
 
 urlpatterns = [
     path("", views.main, name="main"),
+    path("ai-search", views.ai_search, name="ai-search"),
 ]
--- core/app_main/views.py
+++ core/app_main/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def main(request):
     return render(request, "app_main/index.html", context={})
 
+def ai_search(request):
+    return render(request, "app_main/ai-search.html", context={})
