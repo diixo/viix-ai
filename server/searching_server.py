40a3685b971d180de7bb32c53f678b0b7ddac0f7
diixo <anatoliy.podoba@dxc.com>
2025-01-23 04:55:21 +0200

    searching server: implemented operations

--- server/searching_server.py
+++ server/searching_server.py
@@ -1,40 +1,76 @@
 
 import logging
+import json
 from server.smart_search import SmartSearch
 from server.schemas import ContentItemModel
 from typing import List
+from pathlib import  Path
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(threadName)s %(message)s')
-logger = logging.getLogger()
-#logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
 
 
 path_db_index = "server/db-storage/db-index.bin"
+path_db_content = "server/db-storage/db-content.json"
+local_db_json = "json:///db-content"
 
 
 class SearchingServer:
 
     def __init__(self):
         self.smart_search = SmartSearch()
+        self.content = self.open_db()
+        self.load_test()
+
+
+    def open_db(self):
+        if Path(path_db_content).exists() and Path(path_db_index).exists():
+            logging.info("open_db")
+            self.smart_search.open_file(path_db_index)
+            fd = open(path_db_content, 'r', encoding='utf-8')
+            return json.load(fd)
+        return dict()
 
 
     def search(self, str_reuest: str) -> List[ContentItemModel]:
-        logger.info(f"search-request:{str_reuest}")
+        logging.info(f"search-request:{str_reuest}")
         result = []
         indices, distances = self.smart_search.search(str_reuest.lower())
         return result
 
 
     def page_to_index(self, url: str):
-        logger.info(f"page_to_index:{url}")
-        some_text = "some text"
-        self.smart_search.add_str_to_index(some_text)
-        self.smart_search.write_index(path_db_index)
+        logging.info(f"page_to_index:{url}")
+        #self.smart_search.add_str_to_index(some_text)
+        #self.smart_search.write_index(path_db_index)
 
 
     def text_to_index(self, txt: str):
-        logger.info(f"text_to_index:{txt}")
-        self.smart_search.add_str_to_index(txt)
-        self.smart_search.write_index(path_db_index)
+        logging.info(f"text_to_index:{txt}")
+
+        if self.smart_search.add_str_to_index(txt):
+            self.content["content"] = self.content.get("content", list())
+            self.content["content"].append(txt)
+
+            with open(path_db_content, 'w', encoding='utf-8') as fd:
+                json.dump(self.content, fd, ensure_ascii=False, indent=2)
+
+            self.smart_search.write_index(path_db_index)
+
+
+    def load_test(self):
+        test_txts = "server/db-storage/test.txt"
+        if len(self.content) == 0:
+            logging.info(f"load_test load:{test_txts}")
+
+            file_path = Path(test_txts)
+            lines = file_path.read_text(encoding="utf-8").splitlines()
+
+            if self.smart_search.add_texts_to_index(lines):
+                self.content["content"] = self.content.get("content", list())
+                self.content["content"].extend(lines)
+
+                with open(path_db_content, 'w', encoding='utf-8') as fd:
+                    json.dump(self.content, fd, ensure_ascii=False, indent=2)
+
+                self.smart_search.write_index(path_db_index)
